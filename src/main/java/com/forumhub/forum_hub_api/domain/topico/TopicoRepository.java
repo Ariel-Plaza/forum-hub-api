af881fb0c160091edc8a5330453e6143fc840af3
Ariel Plaza <92240996+Ariel-Plaza@users.noreply.github.com>
2026-07-05 15:35:51 -0300

    Add import for Optional in TopicoRepository

--- src/main/java/com/forumhub/forum_hub_api/domain/topico/TopicoRepository.java
+++ src/main/java/com/forumhub/forum_hub_api/domain/topico/TopicoRepository.java
@@ -2,6 +2,7 @@ package com.forumhub.forum_hub_api.domain.topico;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
     Page<Topico> findByActivoTrue(Pageable paginacion);
